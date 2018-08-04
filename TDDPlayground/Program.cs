253bb89c0077a3693c2916ba1fab876b296e52d6
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-08-04 00:38:44 -0400

    Unit tests

--- TDDPlayground/Program.cs
+++ TDDPlayground/Program.cs
@@ -10,6 +10,43 @@ namespace TDDPlayground
     {
         static void Main(string[] args)
         {
+            //TDD Phase
+            int speed;
+            string ticket;
+
+            speed = 0;
+            ticket = SpeedingIfElse.IssueSpeedTicket(speed);
+            Console.WriteLine("input = {0} , output = {1} ", speed, ticket);
+
+            speed = 50;
+            ticket = SpeedingIfElse.IssueSpeedTicket(speed);
+            Console.WriteLine("input = {0} , output = {1} ", speed, ticket);
+
+            speed = 60;
+            ticket = SpeedingIfElse.IssueSpeedTicket(speed);
+            Console.WriteLine("input = {0} , output = {1} ", speed, ticket);
+
+            speed = 61;
+            ticket = SpeedingIfElse.IssueSpeedTicket(speed);
+            Console.WriteLine("input = {0} , output = {1} ", speed, ticket);
+
+            speed = 79;
+            ticket = SpeedingIfElse.IssueSpeedTicket(speed);
+            Console.WriteLine("input = {0} , output = {1} ", speed, ticket);
+
+            speed = 80;
+            ticket = SpeedingIfElse.IssueSpeedTicket(speed);
+            Console.WriteLine("input = {0} , output = {1} ", speed, ticket);
+
+
+            speed = 81;
+            ticket = SpeedingIfElse.IssueSpeedTicket(speed);
+            Console.WriteLine("input = {0} , output = {1} ", speed, ticket);
+
+            speed = -49;
+            ticket = SpeedingIfElse.IssueSpeedTicket(speed);
+            Console.WriteLine("input = {0} , output = {1} ", speed, ticket);
+
         }
     }
 }
---
+++ TDDPlayground/SpeedingIfElse.cs
@@ -0,0 +1,31 @@
+﻿namespace TDDPlayground
+{
+    internal class SpeedingIfElse
+    {
+        int Speed { get; set; }
+
+
+
+        public static string IssueSpeedTicket(int speed)
+        {
+            string ticket;
+            if (speed <= 60)
+            {
+                ticket = "No ticket";
+            }
+            else if ((speed >= 61) && (speed <= 80))
+            {
+                ticket = "Small ticket";
+            }
+            else
+            {
+                ticket = "Big ticket";
+
+            }
+
+
+            return ticket;
+
+        }
+    }
+}
