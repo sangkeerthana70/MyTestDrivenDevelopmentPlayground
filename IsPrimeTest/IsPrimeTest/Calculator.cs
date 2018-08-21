a3311c96f781dded92ebe099f4e697d4d0a13774
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-08-21 18:06:16 -0400

    Calculator Kata

---
+++ IsPrimeTest/IsPrimeTest/Calculator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Calculator
+    {
+        //property
+        public int Num1 { get; set; }
+        public int Num2 { get; set; }
+
+        //empty constructor
+        public Calculator()
+        {
+            
+        }
+        //method
+        public int addNumbers(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public int subtractNumbers(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        
+        
+            
+
+
+
+        
+    }
+
+    
+}
---
+++ IsPrimeTest/IsPrimeTest/CalculatorTest.cs
@@ -0,0 +1,61 @@
+﻿using NUnit.Framework;
+using Calculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class CalculatorTest
+    {
+        Calculator calculator;
+        [SetUp]
+        public void SetUp()
+        {
+             calculator = new Calculator();
+        }
+        
+
+        [Test]
+        public void addReturnsInteger()
+        {
+            //Calculator calculator = new Calculator();
+            var actual = calculator.addNumbers(2, 3);
+            int expected = 5;
+            Assert.IsTrue(actual.GetType() == expected.GetType(), "Result should be of type integer");
+        }
+        [TestCase(7,97)]
+        [TestCase(15, 25)]
+        public void addResultGreaterThanInput(int n1, int n2)
+        {
+            //Calculator calculator = new Calculator();
+            var actual = calculator.addNumbers(n1,n2);
+            Assert.IsTrue(((actual > n1) && (actual > n2)), "Result should be greater than individual numbers");
+        }
+        [TestCase(7, 97)]
+        [TestCase(15, 25)]
+        [TestCase(7, -97)]
+        //this method checks for adding two zeros as well
+        public void addTwoPositiveIntegers(int n1, int n2)
+        {
+            
+            var actual = calculator.addNumbers(n1, n2);
+            Console.WriteLine(actual);
+            Assert.IsTrue(actual == (n1 + n2), "Result should return only positive integers");
+        }
+        [TestCase(7, -97)]
+        [TestCase(15, 25)]
+        //this method checks for subtraction
+        public void subtractTwoNumbers(int n1, int n2)
+        {
+
+            var actual = calculator.subtractNumbers(n1, n2);
+            Console.WriteLine(actual);
+            Assert.IsTrue(actual == (n1 - n2), "Result should return only negative integers");
+        }
+
+
+    }
+}
