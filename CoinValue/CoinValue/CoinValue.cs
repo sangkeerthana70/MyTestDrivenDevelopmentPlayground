b0fae893e91bc69787168c3ca511418d721c373b
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-08-22 15:27:52 -0400

    Coin Value Tdd Example

---
+++ CoinValue/CoinValue/CoinValue.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinValue
+{
+    class Coin
+    {
+        //property
+        public string coin { get; set; }
+        //empty constructor
+        public Coin()
+        {
+
+        }
+        //methods
+        public int GetCoinValue(string coin)
+        {
+            int result = -1;
+            if (coin == "Quarter")
+            {
+                result = 25;
+            }
+            return result;
+            /*
+            else if (coin == "Dime")
+            {
+                return 10;
+            }
+            else if (coin == "Nickel")
+            {
+
+            }*/
+        }
+    }
+
+    
+
+
+}
---
+++ CoinValue/CoinValue/CoinValueTest.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinValue
+{
+    class CoinValueTest
+    {
+        //global variable
+        Coin coin;
+        [SetUp]
+        public void SetUp()
+        {
+            coin = new Coin();
+        }
+        [TestCase("Zero")]
+        public void ReturnsInteger(string input)
+        {
+            var actual = coin.GetCoinValue(input);
+            var expected = -1;
+            Console.WriteLine(actual.GetType());
+            Assert.AreEqual(actual.GetType(), expected.GetType(),"Result should be of type integer");
+        }
+        [TestCase("Quarter")]
+        public void QuarterReturns25(string input)
+        {            
+                var actual = coin.GetCoinValue(input);
+                var expected = 25;
+                Console.WriteLine(actual);
+                Assert.AreEqual(actual, expected, "Result should return an integer");           
+
+        }
+
+
+    }
+}
