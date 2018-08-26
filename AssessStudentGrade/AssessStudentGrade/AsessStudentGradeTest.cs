7221faf8b9bdc9d2ce30a025af366a3b33037f9b
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-08-26 16:45:52 -0400

    ASsess student grade

---
+++ AssessStudentGrade/AssessStudentGrade/AsessStudentGradeTest.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssessStudentGrade
+{
+    class AsessStudentGradeTest
+    {
+        AssessStudentGrade grade;
+        [SetUp]
+        public void Setup()
+        {
+            grade = new AssessStudentGrade();
+        }
+        [TestCase(97)]
+        [TestCase(98)]
+        [TestCase(99)]
+        [TestCase(100)]
+        public void Int97ReturnsGradeAPlus(int score)
+        {
+            var actual = grade.CalcGrade(score);
+            var expected = "A+";
+            Console.WriteLine(actual.GetType());
+            Assert.AreEqual(actual.GetType(), expected.GetType(), "Result should be a string");
+
+        }
+        [TestCase(92)]
+        [TestCase(93)]
+        [TestCase(94)]
+        [TestCase(95)]
+        [TestCase(96)]
+        [TestCase(97)]
+        [TestCase(85)]
+        public void Range92to97ReturnsGradeA(int score)
+        {
+            var actual = grade.CalcGrade(score);
+            var expected = "A";
+            Console.WriteLine(actual);
+            Assert.IsTrue(actual == expected, "actual and expected are different");
+        }
+
+
+    }
+}
---
+++ AssessStudentGrade/AssessStudentGrade/AssessStudentGrade.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssessStudentGrade
+{
+    class AssessStudentGrade
+    {
+        //property
+        public int scoreOfStudent { get; set; }
+        //empty constructor
+        public  AssessStudentGrade()
+        {
+
+        }
+        //methods
+        public string CalcGrade(int score)
+        {
+            string grade = "";
+            if ( score >= 97)
+            {
+                grade = "A+";
+                
+            }
+            if ((score >= 92) && (score <= 97))
+            {
+                grade = "A";
+            }
+            return grade;
+        }
+    }
+}
